5b47ab2ac7dade13880e09e89b4bc5b85c516386
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-26 21:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let P = document.querySelector("#status")
+let btn = document.querySelector("#enterBtn")
+let H1 = document.createElement('h1');
+H1.innerText = `Entered Metaverse`
+btn.addEventListener('click', ()=>{
+	P.innerHTML = ``;
+	P.appendChild(H1);
+})
